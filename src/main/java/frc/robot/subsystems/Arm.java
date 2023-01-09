dde26d1b1ae389af69b146ab9de612d3740a34f2
DylanWhite2246Programming <whited200550@gmail.com>
2023-01-09 13:27:30 -0500

    changed subsytem type of arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,23 +6,33 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj2.command.TrapezoidProfileSubsystem;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 
-public class Arm extends TrapezoidProfileSubsystem {
+public class Arm extends ProfiledPIDSubsystem {
   private static CANSparkMax m1, m2;
   /** Creates a new Arm. */
   public Arm() {
     super(
-        // The constraints for the generated profiles
-        new TrapezoidProfile.Constraints(0, 0),
-        // The initial position of the mechanism
-        0);
+      // The ProfiledPIDController used by the subsystem
+      new ProfiledPIDController(
+        0,
+        0,
+        0,
+        // The motion profile constraints
+        new TrapezoidProfile.Constraints(0, 0)));
     m2.follow(m1);
   }
 
   @Override
-  protected void useState(TrapezoidProfile.State state) {
-    // Use the computed profile state here.
+  public void useOutput(double output, TrapezoidProfile.State setpoint) {
+    // Use the output (and optionally the setpoint) here
+  }
+
+  @Override
+  public double getMeasurement() {
+    // Return the process variable measurement here
+    return 0;
   }
 }
