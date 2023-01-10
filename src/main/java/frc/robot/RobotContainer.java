ddc86e38b64d7beed690f62e28f0a5a47664f707
DylanWhite2246Programming <whited200550@gmail.com>
2023-01-10 13:04:05 -0500

    called auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -67,6 +67,6 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     //return Autos.exampleAuto(m_exampleSubsystem);
-    return null;
+    return autonChooser.getSelected();
   }
 }
