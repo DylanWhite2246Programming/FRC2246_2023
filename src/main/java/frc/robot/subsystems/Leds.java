2dd24c3630b006349fba8a7e0d2d8539f297172c
DylanWhite2246Programming <whited200550@gmail.com>
2023-01-19 14:31:19 -0500

    added methods to control color

--- src/main/java/frc/robot/subsystems/Leds.java
+++ src/main/java/frc/robot/subsystems/Leds.java
@@ -6,7 +6,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.Ports;
 
 public class Leds extends SubsystemBase {
@@ -14,12 +18,45 @@ public class Leds extends SubsystemBase {
   private static AddressableLEDBuffer ledBuffer;
   /** Creates a new Leds. */
   public Leds() {
-    leds=new AddressableLED(Ports.kLedPort);
+    leds = new AddressableLED(Ports.kLedPort);
     ledBuffer = new AddressableLEDBuffer(60);//TODO change to led length
     leds.setLength(ledBuffer.getLength());
     leds.setData(ledBuffer);
     leds.start();
   }
+
+  /**sets all leds in the strip to the color provided */
+  public static void setColor(Color color){
+    for(int i=0;i<ledBuffer.getLength();i++){
+      ledBuffer.setLED(i, color);
+    }
+    leds.setData(ledBuffer);
+  }
+  /**toggles the color of the led strip(s) from yellow and purple 
+   * with yellow being the default color
+   */
+  public CommandBase toggleColor(){
+    return runOnce(()->{
+        if(ledBuffer.getLED(0)==Color.kYellow){setColor(Color.kPurple);}
+        else{setColor(Color.kYellow);}
+      }
+    );
+  }
+  public CommandBase flashLeds(){
+    return new SequentialCommandGroup(
+      turnOnLeds(),
+      new WaitCommand(.2),
+      turnOffLeds(),
+      new WaitCommand(.2),
+      turnOnLeds(),
+      new WaitCommand(.2),
+      turnOffLeds(),
+      new WaitCommand(.2),
+      turnOnLeds()
+    );
+  }
+  public CommandBase turnOnLeds(){return runOnce(()->leds.start());}
+  public CommandBase turnOffLeds(){return runOnce(()->leds.stop());}
 
   @Override
   public void periodic() {
