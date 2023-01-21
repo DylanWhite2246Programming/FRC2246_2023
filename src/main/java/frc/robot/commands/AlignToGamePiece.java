7633c500e384621a8900131b8ba1789ee432d4b7
DylanWhite2246Programming <whited200550@gmail.com>
2023-01-20 19:31:52 -0500

    made work

--- src/main/java/frc/robot/commands/AlignToGamePiece.java
+++ src/main/java/frc/robot/commands/AlignToGamePiece.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Vision;
@@ -16,9 +17,10 @@ public class AlignToGamePiece extends CommandBase {
   Drivetrain drivetrain;
   Vision vision;
   double error;
+  PIDController turnController = new PIDController(.1, 0, 0); //TODO tune
   /**
    * @param mag forward component
-   * @param piece the game piece to target
+   * @param piece the game piece to target 1 for cone 2 for cube
    * @param drive drive train
    */
   public AlignToGamePiece(
@@ -31,7 +33,7 @@ public class AlignToGamePiece extends CommandBase {
     this.piece = piece;
     this.drivetrain = drive;
     this.vision = vision;
-    addRequirements(drive,vision);
+    addRequirements(drive, vision);
   }
 
   // Called when the command is initially scheduled.
@@ -43,7 +45,11 @@ public class AlignToGamePiece extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(vision.getResults().hasTargets()){}
+    if(vision.getResults().hasTargets()){
+      drivetrain.operatorDrive(mag, ()->turnController.calculate(vision.getResults().getBestTarget().getYaw(), 0)).schedule();
+    }else{
+      drivetrain.operatorDrive(mag, ()->0).schedule();
+    }
   }
 
   // Called once the command ends or is interrupted.
